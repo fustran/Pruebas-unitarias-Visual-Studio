9ddb7725f3d6e42f95d304e68cd922a884254f8c
fustran <149261216+fustran@users.noreply.github.com>
2024-01-28 18:56:25 +0100

    Primer commit en la rama main

---
+++ gestionBancariaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("gestionBancariaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gestionBancariaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("47c9822d-655f-49e6-85ed-cf242fefa4f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ gestionBancariaTest/gestionBancariaTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using gestionBancariaApp;
+
+namespace gestionBancariaTest
+{
+    [TestClass]
+    public class gestionBancariaTests
+    {
+        [TestMethod]
+        public void validarMetodoIngreso()
+        {
+            // preparación del caso de prueba
+            double saldoInicial = 1000;
+            double ingreso = 500;
+            double saldoActual = 0;
+            double saldoEsperado = 1500;
+            gestionBancaria cuenta = new gestionBancaria(saldoInicial);
+            // Método a probar
+            cuenta.realizarIngreso(ingreso);
+            // afirmación de la prueba (valor esperado Vs. Valor obtenido)
+            saldoActual = cuenta.obtenerSaldo();
+            Assert.AreEqual(saldoEsperado, saldoActual, 0.001, "El saldo de la cuenta no es correcto");
+        }
+
+        [TestMethod]
+        public void validarMetodoReintegro()
+        {
+            // preparación del caso de prueba
+            double saldoInicial = 1000;
+            double reintegro = 500;
+            double saldoActual = 0;
+            double saldoEsperado = 500;
+            gestionBancaria cuenta = new gestionBancaria(saldoInicial);
+            // Método a probar
+            cuenta.realizarReintegro(reintegro);
+            // afirmación de la prueba (valor esperado Vs. Valor obtenido)
+            saldoActual = cuenta.obtenerSaldo();
+            Assert.AreEqual(saldoEsperado, saldoActual, 0.001, "El saldo de la cuenta no es correcto");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ValidarExcepcionRealizarIngresoCantidadNegativa()
+        {
+            // preparación del caso de prueba
+            double saldoInicial = 1000;
+            double ingreso = -1;
+            gestionBancaria cuenta = new gestionBancaria(saldoInicial);
+            // Método a probar
+            cuenta.realizarIngreso(ingreso);
+            // afirmación de la prueba (valor esperado Vs. Valor obtenido)
+            cuenta.obtenerSaldo();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ValidarExcepcionReintegroMayorQueSaldo()
+        {
+            // preparación del caso de prueba
+            double saldoInicial = 1000;
+            double reintegro = 1001;
+            gestionBancaria cuenta = new gestionBancaria(saldoInicial);
+            // Método a probar
+            cuenta.realizarReintegro(reintegro);
+            // afirmación de la prueba (valor esperado Vs. Valor obtenido)
+            cuenta.obtenerSaldo();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ValidarExcepcionReintegroCantidadNegativa()
+        {
+            // preparación del caso de prueba
+            double saldoInicial = 1000;
+            double reintegro = -1;
+            gestionBancaria cuenta = new gestionBancaria(saldoInicial);
+            // Método a probar
+            cuenta.realizarReintegro(reintegro);
+            // afirmación de la prueba (valor esperado Vs. Valor obtenido)
+            cuenta.obtenerSaldo();
+        }
+    }
+}
